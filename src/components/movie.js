9c3e03aaebbf1df8c2aee263a703928e7e736274
Erin Tait <erinmtait@gmail.com>
2018-11-12 17:32:28 -0800

    Finished Axios Prototype

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Movie(props) {
+    console.log('Movie info: ', props.info);
+
+    const {info} = props;
     return (
         <div>
-            <h3>Movie</h3>
+            <img src={info['im:image'][2].label}/>
+            <h3>{info['im:name'].label}</h3>
+            <p>{info.summary.label}</p>
         </div>
     )
 }
--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -3,17 +3,35 @@ import Movie from './movie';
 import axios from 'axios';
 
 class MovieContainer extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        };
+    }
+
     componentWillMount(){
         const api_url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json'
         axios.get(api_url).then((response) => {
             console.log('Response', response);
+
+            this.setState({
+                movies: response.data.feed.entry
+            });
         });
     }
+
     render(){
+        console.log(this.state);
+
+        const movieList = this.state.movies.map((movieInfo, index) => {
+            return <Movie info={movieInfo} key={index}/>;
+        });
+
         return (
             <div>
-                <h2>Movie Container</h2>
-                <Movie/>
+                {movieList}
            </div>
         )
     }
