50847709d6833adec1750d9f2dabb03e024953a9
Erin Tait <erinmtait@gmail.com>
2018-11-23 02:24:16 -0800

    completed routing intro prototype for mboutique setup

---
+++ src/components/our_macarons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            <h1>Our Macarons</h1>
+            <p>stuff about macarons</p>
+        </div>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut diam sed diam porttitor convallis. Morbi pharetra pretium tortor eu convallis. Praesent hendrerit finibus imperdiet. Aliquam eu massa a arcu sagittis malesuada. Nunc bibendum dolor id venenatis tristique. Fusce porta placerat sapien, eget rutrum turpis. Aenean mattis, odio ut rutrum bibendum, tellus diam mollis erat, vel egestas augue ipsum in felis. Vestibulum blandit, ipsum eget varius porta, sem lectus viverra ligula, eget maximus nibh ligula at nisi. Duis ut nisi luctus erat maximus hendrerit eu eu ante. Proin molestie cursus ipsum id gravida. Aenean non augue nec ligula convallis suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+        </div>
+    )
+}
